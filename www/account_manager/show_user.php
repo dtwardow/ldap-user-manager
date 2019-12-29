2510494acdeddc44165bc2538c2a814fae7b4821
Dennis Twardowsky <twardowsky@gmail.com>
2019-12-29 17:30:58 +0100

    Fixed wrong paths to internal functions

--- www/account_manager/show_user.php
+++ www/account_manager/show_user.php
@@ -2,7 +2,7 @@
 
 include_once __DIR__ . "/../includes/web_functions.inc.php";
 include_once __DIR__ . "/../includes/ldap_functions.inc.php";
-include_once __DIR__ . "/../includes/module_functions.inc.php";
+include_once __DIR__ . "/module_functions.inc.php";
 set_page_access("admin");
 
 render_header();
